f331174abeb962d3cc3da24b1cb13abb13333039
Liam Lorentzen <lorentzenliam1@gmail.com>
2024-09-09 22:02:16 -0500

    Delete JohnnyCash/JohnnyCash directory
    fixed directories

--- JohnnyCash/JohnnyCash/src/main/java/com/liam/JohnnyCash/repo/SongRepo.java
+++
@@ -1,9 +0,0 @@
-package com.liam.JohnnyCash.repo;
-
-import com.liam.JohnnyCash.model.Song;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SongRepo extends JpaRepository<Song, Long> {
-
-
-}
