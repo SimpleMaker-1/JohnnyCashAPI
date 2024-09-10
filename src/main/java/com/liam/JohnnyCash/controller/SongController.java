9148bfb5ff8b1e59b9e91e846b29c2f735130f61
Liam Lorentzen <lorentzenliam1@gmail.com>
2024-09-10 13:07:08 -0500

    Add files via upload
    forgot to change the date method. it was just calling the album method in repo. fixed here hopefully.

--- src/main/java/com/liam/JohnnyCash/controller/SongController.java
+++ src/main/java/com/liam/JohnnyCash/controller/SongController.java
@@ -52,7 +52,7 @@ public class SongController {
     public List<Song> getSongsByDate(@PathVariable String token, @PathVariable String date){
         String fixedDate= date.replace(',','/'); //you cannot have / in the URL path so im substituting commas
         if(token.equals(System.getenv("token")) || token.equals(System.getenv("token2"))){
-            return songRepo.findByAlbum(fixedDate);
+            return songRepo.findByDate(fixedDate);
         }
         else{
             return null;
